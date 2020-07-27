f5aaff102f1146afdbdc1f330debb017c4376f43
Shifu353 <68873489+Shifu353@users.noreply.github.com>
2020-07-27 17:41:59 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,68 +1,32 @@
 from __main__ import app
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy(app)
+basedatos=SQLAlchemy(app)
 
-class Usuarios(db.Model):
-    __nametable__ = "Usuarios"
-    dni = db.Column(db.Integer, primary_key=True)
-    clave = db.Column(db.String(100), nullable=False)
-    tipo = db.Column(db.String(100), nullable=False)
+class Productos(basedatos.Model):
+    NumProducto= basedatos.Column(basedatos.Integer,primary_key=True)
+    Nombre= basedatos.Column(basedatos.String(120),nullable=False)
+    PrecioUnitario= basedatos.Column(basedatos.Float,nullable=False)
+    item_producto=basedatos.relationship('ItemsPedidos',backref='productos',lazy='dynamic')
+class ItemsPedidos(basedatos.Model):
+    NumItem= basedatos.Column(basedatos.Integer,primary_key=True)
+    NumPedido=basedatos.Column(basedatos.Integer,basedatos.ForeignKey('pedidos.NumPedido'))
+    NumProducto=basedatos.Column(basedatos.Integer,basedatos.ForeignKey('productos.NumProducto'))
+    Precio=basedatos.Column(basedatos.Float,nullable=False)
+    Estado=basedatos.Column(basedatos.String(50),nullable=False)
 
-    def Verificar (self,pas):
-        if self.dni == pas:
-            return True
-        return False
+class Pedidos(basedatos.Model):
+    NumPedido= basedatos.Column(basedatos.Integer,primary_key=True)
+    Fecha= basedatos.Column(basedatos.DateTime)
+    Total= basedatos.Column(basedatos.Float,nullable=False)
+    Cobrado= basedatos.Column(basedatos.String(2),nullable=False)
+    Observacion=basedatos.Column(basedatos.Text)
+    DniMozo=basedatos.Column(basedatos.Integer,basedatos.ForeignKey('usuarios.DNI'))
+    Mesa=basedatos.Column(basedatos.Integer,nullable=False,unique=True)
+    item_pedido=basedatos.relationship('ItemsPedidos',backref='pedidos',lazy='dynamic')
 
-class Pedidos(db.Model):
-    numPedido = db.Column(db.Integer, primary_key=True)
-    fecha = db.Column(db.DateTime, nullable=False)
-    total = db.Column(db.Float, nullable=False)
-    cobrado = db.Column(db.Boolean, nullable=False)
-    observacion = db.Column(db.Text)
-    Mesa = db.Column(db.Integer, nullable=False)
-    dnimozo = db.Column(db.Integer, db.ForeignKey('Usuarios.DNI'))
-
-class ItemsPedidos(db.Model):
-    __nametable__="ItemPedido"
-    numItem = db.Column(db.Integer, primary_key=True)
-    numPedido = db.Column(db.Integer, db.ForeignKey('Pedidos.NumPedido'))
-    NumProducto = db.Column(db.Integer, db.ForeignKey('Productos.NumProducto'))
-    precio = db.Column(db.Float, nullable=False)
-    estado = db.Column(db.String(100), nullable=False)
-
-class Productos(db.Model):
-    __nametable__="Productos"
-    numProducto = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(100), nullable=False)
-    PrecioUnitario = db.Column(db.Float, nullable=False)
-
-
-"""class Usuarios(db.Model):
-    __nametable__ = "Usuario"
-    dni = db.Column(db.Integer, primary_key=True)
-    clave = db.Column(db.String(100), nullable=False)
-    tipo = db.Column(db.String(100), nullable=False)
-
-class Pedidos(db.Model):
-    numPedido = db.Column(db.Integer, primary_key=True)
-    fecha = db.Column(db.DateTime, nullable=False)
-    total = db.Column(db.Float, nullable=False)
-    cobrado = db.Column(db.Boolean, nullable=False)
-    observacion = db.Column(db.Text)
-    Mesa = db.Column(db.Integer, nullable=False)
-    dnimozo = db.Column(db.Integer, db.ForeignKey('Usuarios.DNI'))
-
-class ItemsPedidos(db.Model):
-    __nametable__="ItemPedido"
-    numItem = db.Column(db.Integer, primary_key=True)
-    numPedido = db.Column(db.Integer, db.ForeignKey('Pedidos.NumPedido'))
-    NumProducto = db.Column(db.Integer, db.ForeignKey('Productos.NumProducto'))
-    precio = db.Column(db.Float, nullable=False)
-    estado = db.Column(db.String(100), nullable=False)
-
-class Productos(db.Model):
-    __nametable__="Productos"
-    numProducto = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(100), nullable=False)
-    PrecioUnitario = db.Column(db.Float, nullable=False)"""
+class Usuarios(basedatos.Model):
+    DNI=basedatos.Column(basedatos.String(16),primary_key=True)
+    Clave=basedatos.Column(basedatos.String(16),nullable=False,unique=True)
+    Tipo=basedatos.Column(basedatos.String(16),nullable=False,unique=True)    
+    pedido_cargado = basedatos.relationship('Pedidos',backref='usuarios',lazy='dynamic')
