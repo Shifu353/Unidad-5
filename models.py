8b9574e06dbe2c6696788414bd52907dbe7b06f7
Shifu353 <68873489+Shifu353@users.noreply.github.com>
2020-07-30 17:35:55 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,32 +1,39 @@
 from __main__ import app
 from flask_sqlalchemy import SQLAlchemy
 
+
 basedatos=SQLAlchemy(app)
 
-class Productos(basedatos.Model):
-    NumProducto= basedatos.Column(basedatos.Integer,primary_key=True)
-    Nombre= basedatos.Column(basedatos.String(120),nullable=False)
-    PrecioUnitario= basedatos.Column(basedatos.Float,nullable=False)
-    item_producto=basedatos.relationship('ItemsPedidos',backref='productos',lazy='dynamic')
+class Usuarios(basedatos.Model):
+    __tablename__="usuarios"
+    DNI=basedatos.Column(basedatos.String(16),primary_key=True, nullable=False)
+    Clave=basedatos.Column(basedatos.String(16),nullable=False)
+    Tipo=basedatos.Column(basedatos.String(16),nullable=False)    
+    pedido_cargado = basedatos.relationship('Pedidos',backref='usuarios',cascade="all, delete-orphan",lazy='dynamic')
+
+class Pedidos(basedatos.Model):
+    __tablename__ = "pedidos"
+    NumPedido= basedatos.Column(basedatos.Integer,primary_key=True)
+    Fecha= basedatos.Column(basedatos.DateTime,nullable=False)
+    Total= basedatos.Column(basedatos.Float,nullable=False)
+    Cobrado= basedatos.Column(basedatos.String(22),nullable=False)
+    Observacion=basedatos.Column(basedatos.Text)
+    DniMozo=basedatos.Column(basedatos.Integer,basedatos.ForeignKey('usuarios.DNI'))
+    Mesa=basedatos.Column(basedatos.Integer,nullable=False)
+    item_pedido=basedatos.relationship('ItemsPedidos',backref='pedidos', cascade="all, delete-orphan", lazy='dynamic')
+
 class ItemsPedidos(basedatos.Model):
+    __tablename__ = "ItemsPedidos"
     NumItem= basedatos.Column(basedatos.Integer,primary_key=True)
     NumPedido=basedatos.Column(basedatos.Integer,basedatos.ForeignKey('pedidos.NumPedido'))
     NumProducto=basedatos.Column(basedatos.Integer,basedatos.ForeignKey('productos.NumProducto'))
     Precio=basedatos.Column(basedatos.Float,nullable=False)
     Estado=basedatos.Column(basedatos.String(50),nullable=False)
+    item_producto=basedatos.relationship('Productos',backref='itempedidos')
 
-class Pedidos(basedatos.Model):
-    NumPedido= basedatos.Column(basedatos.Integer,primary_key=True)
-    Fecha= basedatos.Column(basedatos.DateTime)
-    Total= basedatos.Column(basedatos.Float,nullable=False)
-    Cobrado= basedatos.Column(basedatos.String(2),nullable=False)
-    Observacion=basedatos.Column(basedatos.Text)
-    DniMozo=basedatos.Column(basedatos.Integer,basedatos.ForeignKey('usuarios.DNI'))
-    Mesa=basedatos.Column(basedatos.Integer,nullable=False,unique=True)
-    item_pedido=basedatos.relationship('ItemsPedidos',backref='pedidos',lazy='dynamic')
-
-class Usuarios(basedatos.Model):
-    DNI=basedatos.Column(basedatos.String(16),primary_key=True)
-    Clave=basedatos.Column(basedatos.String(16),nullable=False,unique=True)
-    Tipo=basedatos.Column(basedatos.String(16),nullable=False,unique=True)    
-    pedido_cargado = basedatos.relationship('Pedidos',backref='usuarios',lazy='dynamic')
+class Productos(basedatos.Model):
+    __tablename__="productos"
+    NumProducto= basedatos.Column(basedatos.Integer,primary_key=True)
+    Nombre= basedatos.Column(basedatos.String(120),nullable=False)
+    PrecioUnitario= basedatos.Column(basedatos.Float,nullable=False)
+    
